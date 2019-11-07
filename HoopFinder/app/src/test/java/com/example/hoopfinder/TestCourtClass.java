31366d60c07390a13f28c13e58ddf703409820a3
jamiemcg10 <jamie.smart.10@gmail.com>
2019-11-06 23:00:00 -0500

    skeleton test classes added for Court and Notification

---
+++ HoopFinder/app/src/test/java/com/example/hoopfinder/TestCourtClass.java
@@ -0,0 +1,32 @@
+package com.example.hoopfinder;
+
+
+import org.junit.Test;
+
+public class TestCourtClass {
+
+    @Test
+    public void testAddCourt(){
+        // Given - a court name
+        // When - addCourt method is called
+        // Then - court is in db
+
+    }
+
+    @Test
+    public void testAddCourtDuplicate(){
+        // Given - a duplicate court name
+        // When - addCourt method is called
+        // Then - court is in db only once
+
+    }
+
+    @Test
+    public void testDeleteCourt(){
+        // Given - a court name that is in the db
+        // When - deleteCourt method is called
+        // Then - court is removed from db
+
+    }
+
+}
