33dbf10691658a0a4d088820ad29034a73e64a90
jamiemcg10 <jamie.smart.10@gmail.com>
2019-11-10 10:50:46 -0500

    test code for lab 4

--- HoopFinder/app/src/test/java/com/example/hoopfinder/TestCourtClass.java
+++ HoopFinder/app/src/test/java/com/example/hoopfinder/TestCourtClass.java
@@ -1,15 +1,60 @@
 package com.example.hoopfinder;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.junit.Test;
 
 public class TestCourtClass {
 
+    public static void main(String[] args){
+        TestCourtClass test = new TestCourtClass();
+        test.testAddCourt();
+    }
+
     @Test
     public void testAddCourt(){
-        // Given - a court name
+        // Given - a court name and coordinates
+        String courtName = "Test Park Court";
+        double latitude = 49.224524;
+        double longitude = -72.351215;
+
+
         // When - addCourt method is called
+        Court.addCourt(courtName, latitude, longitude);
+
+
         // Then - court is in db
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = database.getReference("Courts");
+
+        ref.equalTo("Test Park Court").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                System.out.println(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
