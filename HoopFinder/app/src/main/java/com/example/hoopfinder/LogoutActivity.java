91e7a8a7add6b944b3127a88b0641de9fc1331e3
ignitez <duoyaozh@usc.edu>
2019-11-14 18:04:19 -0500

    Logout activity bug resolved

--- HoopFinder/app/src/main/java/com/example/hoopfinder/LogoutActivity.java
+++ HoopFinder/app/src/main/java/com/example/hoopfinder/LogoutActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -114,6 +115,7 @@ public class LogoutActivity extends AppCompatActivity {
                 .signOut(this)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(LogoutActivity.this,"Signed out",Toast.LENGTH_LONG).show();
                         // user is now signed out
                         startActivity(new Intent(LogoutActivity.this, firebaseAuth.class));
                         finish();
