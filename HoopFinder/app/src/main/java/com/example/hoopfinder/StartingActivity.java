f27a0fcec6a560e9b78e3f52a44ddc1faad158c8
jamiemcg10 <jamie.smart.10@gmail.com>
2019-11-25 23:20:04 -0500

    fixed StartingActivity and SignupActivity layouts so that they render properly on all devices (hopefully)

--- HoopFinder/app/src/main/java/com/example/hoopfinder/StartingActivity.java
+++ HoopFinder/app/src/main/java/com/example/hoopfinder/StartingActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import java.util.Timer;
 import java.util.TimerTask;
