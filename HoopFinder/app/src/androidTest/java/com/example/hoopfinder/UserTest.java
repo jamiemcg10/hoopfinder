3f432c1f7712c110004ffa47d520df0ad723b02e
myz540 <mike.zhong89@gmail.com>
2019-12-12 19:19:29 -0500

    added unit tests for lab 4

---
+++ HoopFinder/app/src/androidTest/java/com/example/hoopfinder/UserTest.java
@@ -0,0 +1,105 @@
+package com.example.hoopfinder;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.test.core.app.ActivityScenario;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Before
+    public void launchActivity(){
+        ActivityScenario.launch(SignupActivity.class);
+
+    }
+
+    @Test
+    public void testConstructor() {
+        final String user_id = "test_id";
+        final String user_email = "test@test.com";
+        final String user_phone_number = "9999999999";
+        final String user_courtsSubscribedTo = "";
+        final String user_usersSubscribedTo = "";
+
+        User testUser = new User(user_id,
+                user_email,
+                user_phone_number,
+                user_courtsSubscribedTo,
+                user_usersSubscribedTo);
+
+        assertEquals(user_id, testUser.user_id);
+        assertEquals(user_email, testUser.user_email);
+        assertEquals(user_phone_number, testUser.user_phone_number);
+        assertEquals(user_courtsSubscribedTo, testUser.user_courtsSubscribedTo);
+        assertEquals(user_usersSubscribedTo, testUser.user_usersSubscribedTo);
+    }
+
+    @Test
+    public void testGenericSubscribeToCourt() {
+        final String currentUid = "wAhy0JiIicS0B0XJSEewDbAsdT83";
+        final String testCourt = "Boston Common";
+
+        User.genericSubscribeToCourt(testCourt, currentUid);
+
+        // check to make sure they are in DB
+        DatabaseReference dbUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        dbUsers.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                boolean courtSubscribed = false;
+                for (DataSnapshot child : dataSnapshot.getChildren()){
+                    User user = child.getValue(User.class);
+                    if (user.getUser_courtsSubscribedTo().contains(testCourt)){
+                        courtSubscribed = true;
+                    }
+                }
+                assertTrue(courtSubscribed);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+    }
+
+    @Test
+    public void testGenericSubscribeToUser() {
+        final String currentUid = "wAhy0JiIicS0B0XJSEewDbAsdT83";
+        final String testUser = "F7SpyoNZCtZHnU6bAGTNcjCpu";
+
+        User.genericSubscribeToUser(testUser, currentUid);
+
+        // check to make sure they are in DB
+        DatabaseReference dbUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        dbUsers.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                boolean userSubscribed = false;
+                for (DataSnapshot child : dataSnapshot.getChildren()){
+                    User user = child.getValue(User.class);
+                    if (user.getUser_usersSubscribedTo().contains(testUser)){
+                        userSubscribed = true;
+                    }
+                }
+                assertTrue(userSubscribed);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+    }
+}
